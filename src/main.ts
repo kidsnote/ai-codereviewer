2f451bbcac79b19f5db59cea4f245818a5e227a9
snoya <snoya.bang@kidsnote.com>
2024-05-30 21:39:15 +0900

    한국어로 대답하도록 프롬프트 수정

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,6 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
-- I'm
 
 Review the following code diff in the file "${
     file.to
