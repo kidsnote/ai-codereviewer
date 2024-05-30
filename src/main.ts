454db12691e826e82f7672b29aee3059bc351ebf
snoya <snoya.bang@kidsnote.com>
2024-05-30 21:37:58 +0900

    한국어로 대답하도록 프롬프트 수정

--- src/main.ts
+++ src/main.ts
@@ -79,14 +79,14 @@ async function analyzeCode(
 }
 
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
-  return `Your task is to review pull requests. Instructions:
+  return `I am Korean, so you should use Korean instead of English. Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
-- IMPORTANT: Answer me in only Korean.
+- I'm
 
 Review the following code diff in the file "${
     file.to
